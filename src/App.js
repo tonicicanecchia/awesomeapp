66ec21bf7c08dadd61b4a9ff584f23f073268924
toni <cicanecchiatoni@gmail.com>
2021-03-22 15:19:35 +0100

    this is my awesome app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ciao toni do you like react?</h1>
+        <p>yes i like it !</p>
+          Edit <code>src/App.js</code> and save to reload.
+        <p></p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hi fairy
+        </a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
